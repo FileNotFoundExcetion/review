caf8be965e9de36c4d04776c1f6ff8cc3f8c3031
yunfeng <bob@163.com>
2022-06-21 09:12:03 +0800

    test username

--- src/main/java/com/leetcode/review/test/Test01.java
+++ src/main/java/com/leetcode/review/test/Test01.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class Test01 {
     public static void main(String[] args) {
         Map<List,Object> map=new HashMap<>();
-        List<String> strings = Arrays.asList("a", "b");
+     //   List<String> strings = Arrays.asList("a", "b");
         Object objectValue = new Object();
-        map.put(strings,objectValue);
-        System.out.println(map.get(strings));
+        map.put(null,objectValue);
+    //    System.out.println(map.get(strings));
+        System.out.println(map);
     }
 }
